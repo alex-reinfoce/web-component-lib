6d810fc13ed5f364ac385064e4992f998f201953
alex-reinfoce <alex.lichangnan@gmail.com>
2024-11-29 18:01:07 +0800

    chore: css

--- package/component/src/component/hello-world.ts
+++ package/component/src/component/hello-world.ts
@@ -1,8 +1,25 @@
-import { html, LitElement } from 'lit';
+import { css, html, LitElement } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 
 @customElement('hello-world')
 export class HelloWorld extends LitElement {
+  static styles = css`
+    div {
+      display: inline-block;
+      padding: 10px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    @media (prefers-color-scheme: dark) {
+      div {
+        background-color: #333;
+        color: #fff;
+      }
+    }
+  `;
+
   @property()
   name = '';
 
